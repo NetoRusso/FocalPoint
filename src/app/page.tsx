082abf9fea11996fe290cd88dc42212459e8a23d
Neto Russo <netoacrn1@gmail.com>
2024-09-22 01:15:19 -0300

    Adicionando a funcionalidade de nome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
+'use client'
 import Header from "@/components/Header";
+import Modal from "@/components/Modal";
 import Task from "@/components/Task";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [open, setOpen] = useState(true);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUserName(storedUser);
+      setOpen(false);
+    }
+  },[])
+
   return (
     <div>
       <Header
-        userName="Marcus"
+        userName={userName}
       />
     <main className="principal">
       <Task />
     </main>
+    {
+      open && <Modal classe="newUser" onClose={() => setOpen(false) } setUserName={setUserName} userName={userName}/>
 
+    }
     </div>
   );
 }
